5844773a90949d02494d698650815f54c1827d2f
Igor Almeida <prof.igor@colegioinfante.info>
2017-08-31 18:04:15 +0100

    lost track of this last modifications, in suma, just trying to use celery for asyncronous work

---
+++ herd/tasks.py
@@ -0,0 +1,114 @@
+from __future__ import absolute_import, unicode_literals
+from celery import task, shared_task
+from django.contrib.auth.models import User
+from .models import Student, Grade
+from openpyxl import load_workbook
+import zipfile
+import os
+import glob
+import shutil
+from django.core.files.images import ImageFile
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+
+
+@shared_task
+def build_school(x):
+    wb = load_workbook(
+        filename=x,
+        read_only=True
+    )
+    for book in wb:
+        ngrade = '{}'.format(book.title).upper()
+        b = 1
+        grades = {}
+        grades[ngrade] = {}
+        for row in book.rows:
+            grades[ngrade]['{}'.format(row[0].value).rstrip('.')] = {
+                'number': '{}'.format(row[0].value).rstrip('.'),
+                'list_number': b,
+                'name': row[1].value
+            }
+            b += 1
+        [pop_student.delay(grades[ngrade][v], ngrade) for v in grades[ngrade]]
+        for s in grades[ngrade]:
+            try:
+                student_number = int(grades[ngrade][s].get('number'))
+            except ValueError:
+                continue
+            student = Student.objects.filter(
+                number__exact=student_number).first()
+            if '{}'.format(str(student_number)) not in grades[ngrade]:
+                student.grade = Grade.objects.filter(name__exact='EX').first()
+                student.save()
+            print('Aluno {} criado ou atualizado.'.format(s))
+    files = glob.glob('{}[0-9]*.xlsx'.format(settings.MEDIA_ROOT))
+    for f in files:
+        os.remove(f)
+
+
+@shared_task
+def pop_student(x, y):
+    student = x
+    grade = y
+    if not Grade.objects.filter(name__exact=grade).exists():
+        gobj = Grade(name=grade)
+        gobj.save()
+    else:
+        gobj = Grade.objects.get(name=grade)
+    fs = FileSystemStorage()
+    st_name = student.get('name')
+    if not st_name:
+        return 0
+    st_name = st_name.rstrip(' ')
+    f_name = st_name.split(' ', 1)[0]
+    namelist = st_name.split(' ', -1)
+    l_name = namelist[len(namelist) - 1]
+    st_number = student.get('number')
+    username = '{}_{}'.format(f_name, student.get('number'))
+    if not User.objects.filter(username__exact=username).exists():
+        u = User.objects.create_user(
+            username,
+            st_number + '@colegioinfante.info',
+            'aluno' + st_number,
+        )
+        u.first_name = f_name
+        u.last_name = l_name
+        u.save()
+    else:
+        u = User.objects.get(username=username)
+    fname = '{}.jpg'.format(st_number)
+    try:
+        fimg = fs.open(fname, mode='rb')
+    except FileNotFoundError:
+        default = '{}'.format(os.path.join(settings.MEDIA_ROOT,
+                                           'default.jpg'))
+        shutil.copy(default, os.path.join(settings.MEDIA_ROOT, fname))
+        fimg = fs.open(fname, mode='rb')
+    f = ImageFile(fimg)
+    if not Student.objects.filter(number__exact=int(st_number)).exists():
+        s = Student(
+            user=u,
+            number=int(st_number),
+            list_number=student.get('list_number'),
+            grade=gobj,
+            photo=f,
+        )
+        s.save()
+    else:
+        s = Student.objects.get(number=int(st_number))
+        s.grade = gobj
+        s.photo = f
+        s.save()
+    fimg.close()
+    f.close()
+    files = glob.glob('{}{}*.jpg'.format(settings.MEDIA_ROOT, fname))
+    for f in files:
+        os.remove(f)
+
+
+@task(name="create_students")
+def create_students(fzip, xlsx):
+    fjpg = zipfile.ZipFile(fzip, 'r')
+    fjpg.extractall(settings.MEDIA_ROOT)
+    build_school.delay(xlsx)
--- herd/views.py
+++ herd/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from .models import Teacher, Student, Grade
+# from django.contrib.auth.models import User
+from .models import Teacher, Grade
 from .forms import UploadFileForm
 from django.contrib import messages
 from openpyxl import load_workbook
 import zipfile
 import os
-from django.core.files.images import ImageFile
+# import shutil
+# from django.core.files.images import ImageFile
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
+from .tasks import create_students
+import pickle
+from io import BytesIO
+from kombu import serialization
+from django.core.files import File
 
+
+def loads(s):
+    return pickle.load(BytesIO(s))
+
+serialization.register(
+    'my_pickle', pickle.dumps, loads,
+    content_type='application/x-pickle3',
+    content_encoding='binary',
+)
 
 @login_required(login_url='/login/google-oauth2/?next=/')
 def update_grade(request):
@@ -28,87 +43,23 @@ def update_grade(request):
 
 @login_required(login_url='/login/google-oauth2/?next=/')
 def upload_file(request):
-    teacher = Teacher.objects.filter(user__exact=request.user.id).first()
-    if not teacher:
-        messages.error(request, 'Tem de ter\
-            autorização para poder verificar presenças.')
+    if request.user.email != 'prof.igor@colegioinfante.info':
+        messages.error(request, 'Apenas para o prof. Igor!')
         return render(request, 'blank.html')
     form = UploadFileForm(request.POST or None, request.FILES or None)
     if not form.is_valid():
         messages.error(request, 'Ocorreu um erro a validar.')
         return render(request, 'herd/upload.html', {'form': form})
     # form is valid:
-    fxlxs = request.FILES['xlxs']
-    fjpg = zipfile.ZipFile(request.FILES['imgzipped'], 'r')
+    # with open(os.path.join(settings.MEDIA_ROOT, 'a.xlxs'), 'wb+') as f:
+    #     fxlxs = File(f)
     fs = FileSystemStorage()
-    wb = load_workbook(filename=fxlxs, read_only=True)
-    school = {}
-    for book in wb:
-        grade = '{}'.format(book.title).upper()
-        b = 1
-        school[grade] = {}
-        for row in book.rows:
-            school[grade]['{}'.format(row[0].value).rstrip('.')] = {
-                'list_number': b,
-                'name': row[1].value
-            }
-            b += 1
-    for grade in school:
-        if not Grade.objects.filter(name__exact=grade).exists():
-            gobj = Grade(name=grade)
-            gobj.save()
-            gobj.refresh_from_db()
-        else:
-            gobj = Grade.objects.get(name=grade)
-        for student in school[grade]:
-            st_name = school[grade][student].get('name')
-            if not st_name:
-                continue
-            st_name = st_name.rstrip(' ')
-            f_name = st_name.split(' ', 1)[0]
-            namelist = st_name.split(' ', -1)
-            l_name = namelist[len(namelist) - 1]
-            username = '{}_{}'.format(f_name, student)
-            if not User.objects.filter(username__exact=username).exists():
-                u = User.objects.create_user(
-                    username,
-                    student + '@colegioinfante.info',
-                    'aluno' + student,
-                )
-                u.first_name = f_name
-                u.last_name = l_name
-                u.save()
-            else:
-                u = User.objects.get(username=username)
-            # storage = DefaultStorage()
-            # f = 'alunos/{}.jpg'.format(student)
-            # f = storage.open(f, mode='rb')
-            fname = '{}.jpg'.format(student)
-            fjpg.extract(fname, settings.MEDIA_ROOT)
-            fimg = fs.open(fname, mode='rb')
-            f = ImageFile(fimg)
-            if not Student.objects.filter(number__exact=int(student)
-                                          ).exists():
-                s = Student(
-                    user=u,
-                    number=int(student),
-                    list_number=school[grade][student].get('list_number'),
-                    grade=gobj,
-                    photo=f,
-                )
-                s.save()
-            else:
-                s = Student.objects.get(number=int(student))
-                s.grade = gobj
-                s.photo = f
-                s.save()
-            fimg.close()
-            f.close()
-            os.remove(os.path.join(settings.MEDIA_ROOT, fname))
-        g = Student.objects.filter(grade=gobj)
-        for s in g:
-            if '{}'.format(s.number) not in school[grade]:
-                s.grade = Grade.objects.filter(name__exact='EX').first()
-                s.save()
+    fx = os.path.join(settings.MEDIA_ROOT,
+                      '0' + request.FILES['xlxs'].name)
+    fz = os.path.join(settings.MEDIA_ROOT,
+                      '0' + request.FILES['imgzipped'].name)
+    fs.save(fx, File(request.FILES['xlxs']))
+    fs.save(fz, File(request.FILES['imgzipped']))
+    create_students(fz, fx)
     messages.success(request, 'Ficheiros enviados com sucesso.')
     return render(request, 'herd/upload.html', {'form': form})
