2694d4bb2919404959a5390eba12d10fab355a0a
Igor Almeida <prof.igor@colegioinfante.info>
2017-09-01 14:52:08 +0100

    some minor modifications just to clear some trash

--- herd/views.py
+++ herd/views.py
@@ -4,28 +4,12 @@ from django.contrib.auth.decorators import login_required
 from .models import Teacher, Grade
 from .forms import UploadFileForm
 from django.contrib import messages
-from openpyxl import load_workbook
-import zipfile
 import os
-# import shutil
-# from django.core.files.images import ImageFile
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 from .tasks import create_students
-import pickle
-from io import BytesIO
-from kombu import serialization
 from django.core.files import File
 
-
-def loads(s):
-    return pickle.load(BytesIO(s))
-
-serialization.register(
-    'my_pickle', pickle.dumps, loads,
-    content_type='application/x-pickle3',
-    content_encoding='binary',
-)
 
 @login_required(login_url='/login/google-oauth2/?next=/')
 def update_grade(request):
@@ -47,12 +31,11 @@ def upload_file(request):
         messages.error(request, 'Apenas para o prof. Igor!')
         return render(request, 'blank.html')
     form = UploadFileForm(request.POST or None, request.FILES or None)
+    if not request.POST:
+        return render(request, 'herd/upload.html', {'form': form})
     if not form.is_valid():
         messages.error(request, 'Ocorreu um erro a validar.')
         return render(request, 'herd/upload.html', {'form': form})
-    # form is valid:
-    # with open(os.path.join(settings.MEDIA_ROOT, 'a.xlxs'), 'wb+') as f:
-    #     fxlxs = File(f)
     fs = FileSystemStorage()
     fx = os.path.join(settings.MEDIA_ROOT,
                       '0' + request.FILES['xlxs'].name)
