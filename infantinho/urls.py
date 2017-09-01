e0d81401badb14a976a1b725d49f32a38c198a4c
Igor Almeida <prof.igor@colegioinfante.info>
2017-09-01 15:14:48 +0100

    starting to work in the achievements and updating link to robots.txt

--- infantinho/urls.py
+++ infantinho/urls.py
@@ -33,4 +33,5 @@ urlpatterns = [
     url('', include('social_django.urls', namespace='social')),
     url('', include('django.contrib.auth.urls', namespace='auth')),
     url(r'^terms/$', TemplateView.as_view(template_name="terms.txt", content_type="text/plain"), name="terms"),
+    url(r'^robots.txt$', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots"),
 ]
