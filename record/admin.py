79ff742ee8236ccdacd750de9abebde01f0df672
Igor Almeida <prof.igor@colegioinfante.info>
2017-09-19 00:28:56 +0100

    adding admin stuff to check records

--- record/admin.py
+++ record/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import PresRec
 
-# Register your models here.
+
+class PresenceModelAdmin(admin.ModelAdmin):
+    """docstring for PresenceModelAdmin"""
+    list_display = ['subject', 'date', 'student', 'is_absent']
+    list_display_links = ['subject', 'date', 'student']
+    list_filter = ['subject', 'date', 'student']
+    search_fields = ['subject', 'date', 'student']
+
+    class Meta:
+        model = PresRec
+
+
+admin.site.register(PresRec, PresenceModelAdmin)
